f85e9602a337c5dea3934a4bbbdb155730715155
XiaoWen Su <2014863935@qq.com>
2023-02-19 21:46:41 +0800

    Hello, Second Commit

--- src/com/siwen/gitclass/Demo.java
+++ src/com/siwen/gitclass/Demo.java
@@ -15,5 +15,6 @@ public class Demo {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
     }
 }
