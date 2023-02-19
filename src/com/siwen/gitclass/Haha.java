816f5b9088c86fad4372e19a15f4c3b008429c68
XiaoWen Su <2014863935@qq.com>
2023-02-19 22:51:46 +0800

    add Haha class

---
+++ src/com/siwen/gitclass/Haha.java
@@ -0,0 +1,14 @@
+package com.siwen.gitclass;
+
+/**
+ * @projectName: IdeaGItDemo
+ * @package: com.siwen.gitclass
+ * @className: Haha
+ * @author: 749291
+ * @description: TODO
+ * @date: 2/19/2023 10:50 PM
+ * @version: 1.0
+ */
+
+public class Haha {
+}
