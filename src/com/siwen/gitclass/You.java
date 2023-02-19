8c332c1b7f93307dd26897095d6bfb1f06419835
XiaoWen Su <2014863935@qq.com>
2023-02-19 21:59:15 +0800

    Add two classes: He and You

---
+++ src/com/siwen/gitclass/You.java
@@ -0,0 +1,14 @@
+package com.siwen.gitclass;
+
+/**
+ * @projectName: IdeaGItDemo
+ * @package: com.siwen.gitclass
+ * @className: You
+ * @author: 749291
+ * @description: TODO
+ * @date: 2/19/2023 9:58 PM
+ * @version: 1.0
+ */
+
+public class You {
+}
