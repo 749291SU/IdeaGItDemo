199a63477b30fb9950d7596689d80efc6e63378a
XiaoWen Su <2014863935@qq.com>
2023-02-19 21:29:26 +0800

    Hello, First Commit

---
+++ src/com/siwen/gitclass/Demo.java
@@ -0,0 +1,17 @@
+package com.siwen.gitclass;
+
+/**
+ * @projectName: IdeaGItDemo
+ * @package: com.siwen.gitclass
+ * @className: Demo
+ * @author: 749291
+ * @description: TODO
+ * @date: 2/19/2023 9:19 PM
+ * @version: 1.0
+ */
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
