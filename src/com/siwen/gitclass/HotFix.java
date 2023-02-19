ca3a486bdee6ab75077330d2efdb20ab3eb7aed2
XiaoWen Su <2014863935@qq.com>
2023-02-19 22:10:39 +0800

    create HotFix class

---
+++ src/com/siwen/gitclass/HotFix.java
@@ -0,0 +1,17 @@
+package com.siwen.gitclass;
+
+/**
+ * @projectName: IdeaGItDemo
+ * @package: com.siwen.gitclass
+ * @className: HotFix
+ * @author: 749291
+ * @description: TODO
+ * @date: 2/19/2023 10:10 PM
+ * @version: 1.0
+ */
+
+public class HotFix {
+    public static void main(String[] args) {
+        System.out.println("fix successfully");
+    }
+}
